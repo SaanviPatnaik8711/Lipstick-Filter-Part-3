87debd052f04808c525a93d1c617c99023f8d88d
SaanviPatnaik8711 <115956391+SaanviPatnaik8711@users.noreply.github.com>
2023-07-21 15:39:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    lipstick = loadImage("https://i.postimg.cc/NM8NRm0f/Lip-Filter.png");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+      console.log(results);
+      noseX = results[0].pose.nose.x - 13;
+      noseY = results[0].pose.nose.y + 15;
+      console.log( "nose x = " + results[0].pose.nose.x);
+      console.log( "nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw(){
+      image(video, 0, 0, 300, 300);
+      //fill("red");
+      //stroke("red");
+      //circle(noseX, noseY, 20);
+      image(lipstick, noseX, noseY, 30, 20);
+}
+
+function take_snapshot(){
+  save('myFilterImage.png');
+}
